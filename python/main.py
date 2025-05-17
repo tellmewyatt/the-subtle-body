3378f5256db2b41c911760dd08de1c90aa3e2b83
Wyatt Cannon <composer@wyattcannon.com>
2025-05-17 00:18:50 -0400

    Got left and right and working with sound

--- python/main.py
+++ python/main.py
@@ -1,55 +1,64 @@
 from model import create_landmarker
 from argparse import ArgumentParser
+import pandas as pd
 import os
 from pythonosc import udp_client
+import itertools
 
 # A list of the landmark indices
-# 0 - nose
-# 1 - left eye (inner)
-# 2 - left eye
-# 3 - left eye (outer)
-# 4 - right eye (inner)
-# 5 - right eye
-# 6 - right eye (outer)
-# 7 - left ear
-# 8 - right ear
-# 9 - mouth (left)
-# 10 - mouth (right)
-# 11 - left shoulder
-# 12 - right shoulder
-# 13 - left elbow
-# 14 - right elbow
-# 15 - left wrist
-# 16 - right wrist
-# 17 - left pinky
-# 18 - right pinky
-# 19 - left index
-# 20 - right index
-# 21 - left thumb
-# 22 - right thumb
-# 23 - left hip
-# 24 - right hip
-# 25 - left knee
-# 26 - right knee
-# 27 - left ankle
-# 28 - right ankle
-# 29 - left heel
-# 30 - right heel
-# 31 - left foot index
-# 32 - right foot index
+landmark_list = [
+    "nose",
+    "left eye (inner)",
+    "left eye",
+    "left eye (outer)",
+    "right eye (inner)",
+    "right eye",
+    "right eye (outer)",
+    "left ear",
+    "right ear",
+    "mouth (left)",
+    "mouth (right)",
+    "left shoulder",
+    "right shoulder",
+    "left elbow",
+    "right elbow",
+    "left wrist",
+    "right wrist",
+    "left pinky",
+    "right pinky",
+    "left index",
+    "right index",
+    "left thumb",
+    "right thumb",
+    "left hip",
+    "right hip",
+    "left knee",
+    "right knee",
+    "left ankle",
+    "right ankle",
+    "left heel",
+    "right heel",
+    "left foot index",
+    "right foot index"
+]
 
 def main(udp_ip, udp_port):
 
     osc_client = udp_client.SimpleUDPClient(udp_ip, udp_port)
     def tick(result, image, timestamp):
 
-        os.system("clear")
         if len(result.pose_world_landmarks) > 0:
             landmarks = result.pose_world_landmarks[0]
-            for landmark in landmarks:
-                print(landmark)
-            osc_client.send_message("/right_wrist_x", landmarks[16].x)
-            osc_client.send_message("/right_wrist_y", landmarks[16].x)
+            df = pd.DataFrame([{
+                "name": landmark_list[i],
+                "x": landmark.x,
+                "y": landmark.y,
+                "z": landmark.z
+            } for i, landmark in enumerate(landmarks)])
+            print(df)
+            print("\033[F" * 35)
+            # Send landmarks as a flat list of x,y,z pairs
+            osc_client.send_message("/pose_landmarks", list(itertools.chain.from_iterable([(landmark.x, landmark.y, landmark.z) for landmark in landmarks])))
         
     create_landmarker(tick)
 if __name__ == "__main__":
@@ -58,3 +67,4 @@ if __name__ == "__main__":
     parser.add_argument("--port", type=int, default=5005, help="The port the OSC server is listening on")
     args = parser.parse_args()
     main(args.ip, args.port)
+    os.system("clear")
