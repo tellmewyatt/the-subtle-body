9795b581e6f04dfdb5045c13c1bca7e3dfecb668
Wyatt Cannon <composer@wyattcannon.com>
2025-09-02 22:01:38 -0400

    Removed python

--- python/astral_body/astral_body/__init__.py
+++
@@ -1,3 +0,0 @@
-import os
-PROJECT_PATH = os.path.dirname(os.path.realpath(__file__)).split(".venv")[0]
-PYTHON_ENV = os.path.join(PROJECT_PATH, ".venv/bin/python")
--- python/astral_body/astral_body/osc_server.py
+++
@@ -1,3 +0,0 @@
-
-from pythonosc import udp_client
-def get_osc_client(ip="127.0.0.1", port=5005):
