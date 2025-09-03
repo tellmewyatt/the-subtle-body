d3f877244d4f1e28c3e876348747713ba74e2764
Wyatt Cannon <composer@wyattcannon.com>
2025-09-02 22:31:27 -0400

    Added open scirptt to start.sh

--- js/astral_body/start.sh
+++ js/astral_body/start.sh
@@ -1,9 +1,14 @@
-NODE_ENV=production node server.js & 
 SC_FILE='../../supercollider/main.scd'
-
+NODE_ENV=production node server.js &
+NODE_PID=$!
+sleep 1
+open http://localhost:3000 &
+OPEN_PID=$!
 sclang $SC_FILE ||
 /Applications/SuperCollider.app/Contents/MacOS/sclang $SC_FILE ||
 /Applications/SuperCollider/SuperCollider.app/Contents/MacOS/sclang $SC_FILE ||
 echo "SuperCollider binary not found! I checked some common locations for it for mac, but its not there.\n
 You need to add it to your path"
-  && kill $!
+SUPERCOLLIDER_PID=$!
+wait
+KILL $NODE_PID $OPEN_PD $SUPERCOLLIDER_PID
